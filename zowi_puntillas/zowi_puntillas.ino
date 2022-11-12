7529ef0da706be2364487cc62143069b663e07c3
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2022-11-12 14:57:24 +0100

    Añadir soporte con Button2
    Debe instalarse el repo Button2

--- zowi_puntillas/zowi_puntillas.ino
+++ zowi_puntillas/zowi_puntillas.ino
@@ -1,6 +1,10 @@
 
-
+#include <Button2.h>
 #include <Servo.h>
+
+#define PIN_PULS 12;
+
+Button2 puls{PIN_PULS};
 
 Servo myservo_pi;
 Servo myservo_pd;
@@ -8,7 +12,29 @@ Servo myservo_pd;
 int vel = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+  // puls.setChangedHandler(changed);
+  // puls.setPressedHandler(pressed);
+  puls.setReleasedHandler(onReleaseDelay); // Retrasa la entrada del ciclo
+
+  // puls.setTapHandler(tap);
+  puls.setClickHandler(piesPuntillas);
+  // puls.setLongClickDetectedHandler(longClickDetected);
+  // puls.setLongClickHandler(longClick);
+  
+  // puls.setDoubleClickHandler(doubleClick);
+  // puls.setTripleClickHandler(tripleClick);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  puls.loop();
+}
+
+void onReleaseDelay() {
+  delay(500);
+}
+
+void piesPuntillas() {
   myservo_pi.attach(9);
   myservo_pd.attach(5);
 
@@ -45,9 +71,4 @@ void setup() {
   myservo_pi.write(vel);
   myservo_pd.write(vel);
   delay(500);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
 }
